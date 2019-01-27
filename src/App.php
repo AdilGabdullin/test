04934a41a69976b7a638f2fad2208ac77de69128
Adil Gabdullin <izenbard@gmail.com>
2019-01-27 16:26:45 +0600

    1.0

---
+++ src/App.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Adildev;
+
+class App
+{
+    public function sayHello()
+    {
+        echo 'Hello, World!' . PHP_EOL;
+    }
+
+    public function sayYo()
+    {
+        echo 'Yo' . PHP_EOL;
+    }
+}
